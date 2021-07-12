e233782e78b503cf83d7931484f16daae780acb4
Foreverender <87213367+Foreverender@users.noreply.github.com>
2021-07-12 12:01:41 -0400

    Update Scene2.js

--- Scene2.js
+++ Scene2.js
@@ -9,7 +9,7 @@ class Scene2 extends Phaser.Scene {
     background.setOrigin(0, 0);
     background.setDepth(2);
     
-    this.add.text(20, 20, 'Click to move differently', { fill: 'white' })
+    var text = this.add.text(20, 20, 'Click to move differently', { fill: 'white' })
 
     var particles = this.add.particles('red');
     var particles2 = this.add.particles('yellow');
